50088d18b6d7a979c1551223113a53995b046937
Oana Donose <donoseoana@gmail.com>
2019-03-10 17:26:26 +0000

    added the 3 main pages with gatsby links

---
+++ src/pages/404.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default () => (
+  <div>
+    <h1>Page not found</h1>
+    <p>This is the 404 page.</p>
+  </div>
+)
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,8 @@
 import React from "react"
-import { Link } from "gatsby"
 
 export default () => (
   <div>
-    <h1> This is the index page</h1>
+    <h1> This is the about page</h1>
     <Link to="/">Home</Link> | <Link to="/about">About me</Link>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod...

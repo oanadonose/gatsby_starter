82913bb7e484c7f5fc339f6828004ce7fc6917a3
Oana Donose <donoseoana@gmail.com>
2019-03-10 17:41:09 +0000

    incorporated support for css modules in 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,9 @@
 import React from "react"
+import styles from "./404.module.css"
 
 export default () => (
   <div>
-    <h1>Page not found</h1>
-    <p>This is the 404 page.</p>
+    <h1 className={styles.header}>Page not found</h1>
+    <p className={styles.errorMessage}>This is the 404 page.</p>
   </div>
 )

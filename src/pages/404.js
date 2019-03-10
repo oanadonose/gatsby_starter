76fcc8a423ecd00e3804a22b2dad725ad4a2bf96
Oana Donose <donoseoana@gmail.com>
2019-03-10 18:07:22 +0000

    added sass gatsby plugin

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,8 @@
 import React from "react"
-import styles from "./404.module.css"
+import styles from "./404.module.scss"
 
 export default () => (
-  <div>
+  <div className={styles.content}>
     <h1 className={styles.header}>Page not found</h1>
     <p className={styles.errorMessage}>This is the 404 page.</p>
   </div>

9d3c74937380a57a076dd1350d6b40fd14047b5b
Oana Donose <donoseoana@gmail.com>
2019-06-13 18:26:38 +0100

    created header, footer and title components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,12 @@
 import React from "react"
 import styles from "./layout.module.scss"
 import Header from "./header.js"
+import Footer from "./footer.js"
 
 export default ({ children }) => (
   <div className={styles.container}>
     <Header />
-    {children}
+    <div className={styles.content}>{children}</div>
+    <Footer>My new Gatsby blog 2019</Footer>
   </div>
 )
